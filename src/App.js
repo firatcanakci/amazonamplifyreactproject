058d49e7cd0f37bb932c958c1eeffd7c7f57205b
Fırat Çanakcı <firat.canakci@flypgs.com>
2020-01-23 16:32:51 +0300

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pegasus Navigation
         </a>
       </header>
     </div>
